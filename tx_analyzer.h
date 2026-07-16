6f6ecb70dbd19d2495226640eee4a03920f081f6
vadymdota2-rgb <vadymdota2@gmail.com>
2026-07-17 01:41:06 +0200

    tx_analyzer.h

--- tx_analyzer.h
+++ tx_analyzer.h
@@ -3,151 +3,30 @@
 #include <string>
 #include <set>
 #include <map>
-#include <vector>
 #include <cstdint>
 #include <boost/multiprecision/cpp_int.hpp>
 #include "json.hpp"
 
 using boost::multiprecision::cpp_int;
 
-enum class TxOperation {
-    UNKNOWN,
-    SWAP_EXACT_IN,
-    SWAP_EXACT_OUT,
-    WRAP_NATIVE,
-    UNWRAP_NATIVE,
-    ADD_LIQUIDITY,
-    REMOVE_LIQUIDITY,
-    TRANSFER,
-    BRIDGE,
-    STAKE,
-    UNSTAKE,
-    CLAIM,
-    NFT
-};
-
-struct DecodedIntent {
-    bool valid = false;
-    bool swap = false;
-    bool exactInput = false;
-    bool exactOutput = false;
-    bool multicall = false;
-    bool universalRouter = false;
-    bool permit2 = false;
-    bool malformed = false;
-    bool bridge = false;
-    bool liquidity = false;
-    bool aggregator = false;
-    bool v4 = false;
-    bool targetedMulticall = false;
-    bool universalSubPlan = false;
-    bool acrossBridge = false;
-    bool v4ActionsDecoded = false;
-    bool standardTokenCall = false;
-    bool approvalCall = false;
-    bool selfCall = false;
-    bool okxDagSwap = false;
-    bool targetedExecutor = false;
-
-    TxOperation operation = TxOperation::UNKNOWN;
-
-    std::string selector;
-    std::string functionName;
-    std::string protocol;
-    std::string router;
-    std::string recipient;
-
-    std::string tokenIn;
-    std::string tokenOut;
-    std::string secondaryToken;
-    std::vector<std::string> path;
-
-    cpp_int amountIn = 0;
-    cpp_int amountOut = 0;
-    cpp_int amountOutMin = 0;
-    cpp_int amountInMax = 0;
-    cpp_int nativeValue = 0;
-
-    std::vector<DecodedIntent> children;
-};
-
 struct TxResult {
-    bool valid = false;
-    bool isSwap = false;
-    bool isBuy = false;
-
-    cpp_int rawAmount = 0;
-    cpp_int usdNanos = 0;
-
+    bool valid, isSwap, isBuy;
+    cpp_int rawAmount, usdNanos;
     std::string tokenAddr;
     std::string venue;
     std::string counterAddr;
-    cpp_int counterAmount = 0;
-
+    cpp_int counterAmount;
     bool hasSwapEvent = false;
     bool isUniversalRouter = false;
     bool isGenericMulticall = false;
     bool hasPermit2Signal = false;
     bool dexActivityDetected = false;
-
     bool lpMintOrBurnSeen = false;
     bool lpPoolIdentitySeen = false;
     bool lpV3EventSeen = false;
-
-    bool calldataDecoded = false;
-    bool calldataSwap = false;
-    bool calldataMatched = false;
-    bool calldataRecovered = false;
-    bool permit2Decoded = false;
-    bool liquidityDecoded = false;
-    bool bridgeDecoded = false;
-    bool aggregatorDecoded = false;
-    bool v4Decoded = false;
-    bool targetedMulticallDecoded = false;
-    bool universalSubPlanDecoded = false;
-    bool acrossBridgeDecoded = false;
-    bool v4ActionsDecoded = false;
-
-    bool transferGraphSeen = false;
-    bool graphPathToPool = false;
-    bool graphPathFromPool = false;
-    bool graphRecovered = false;
-    bool graphAmbiguous = false;
-
-    bool walletSwapRelated = false;
-    bool unrelatedSwapEvent = false;
-
-    bool unsupportedSelector = false;
-    bool classifiedByDirectFlow = false;
-    bool classifiedByCalldata = false;
-    bool classifiedByGraph = false;
-    bool classifiedByFallback = false;
-
-    bool standardTokenCall = false;
-    bool approvalCall = false;
-    bool selfCall = false;
-    bool okxDagSwapDecoded = false;
-    bool targetedExecutorDecoded = false;
-
-    std::string callTarget;
-    std::string decodedSelector;
-    std::string decodedFunction;
-    std::string decodedTokenIn;
-    std::string decodedTokenOut;
     std::string unknownReason;
-};
-
-struct RouterInfo {
-    std::string protocol;
-    std::string version;
-    std::string routerType;
-
-    bool supportsV2 = false;
-    bool supportsV3 = false;
-    bool supportsV4 = false;
-    bool supportsPermit2 = false;
-    bool supportsMulticall = false;
-    bool supportsUniversalRouter = false;
+    bool calldataDecoded = false;
+    bool intentGuided = false;
 };
 
 struct ChainContext {
@@ -157,30 +36,14 @@ struct ChainContext {
     std::string nativeSymbol;
     std::string nativeMarker;
     std::string wrappedNative;
-
     std::set<std::string> baseAssets;
     std::set<std::string> stablecoins;
-
-    // Backward-compatible registry.
     std::map<std::string, std::string> routers;
-
-    // Extended registries for future stages.
-    std::map<std::string, RouterInfo> routerInfo;
-    std::set<std::string> aggregators;
     std::set<std::string> bridges;
-    std::set<std::string> staking;
-    std::set<std::string> permit2Contracts;
-    std::set<std::string> multicallContracts;
-
-    // selector -> human-readable function name / operation hint.
-    std::map<std::string, std::string> selectorNames;
-    std::set<std::string> aggregatorSwapSelectors;
-    std::set<std::string> bridgeSelectors;
 };
 
 const ChainContext& chainCtx();
 void setChainContext(const ChainContext& ctx);
-
 ChainContext makeBscContext();
 ChainContext makeEthereumContext();
 ChainContext makeBaseContext();
@@ -191,8 +54,6 @@ extern const std::string NATIVE_BNB_MARKER;
 
 bool isBaseAsset(const std::string& a);
 bool isStablecoin(const std::string& a);
-bool isNativeAsset(const std::string& a);
-bool isQuoteAsset(const std::string& a);
 std::string lookupRouterLabel(const std::string& addr);
 
 cpp_int parseUint256(const std::string& h);
@@ -203,10 +64,4 @@ std::string formatUsd(const cpp_int& n);
 cpp_int calcUnitPriceNanos(const cpp_int& usdNanos, const cpp_int& rawAmount, int dec);
 std::string formatPriceUsd(const cpp_int& n);
 
-DecodedIntent decodeTransactionInput(const nlohmann::json& tx);
-
-TxResult analyzeTx(
-    const nlohmann::json& tx,
-    const nlohmann::json& receipt,
-    const std::string& walletAddress
-);
+TxResult analyzeTx(const nlohmann::json& tx, const nlohmann::json& receipt, const std::string& wa);
