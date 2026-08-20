e6f31c56ed4935dc088a0c160c363d28e63026eb
vadymdota2-rgb <vadymdota2@gmail.com>
2026-08-20 23:00:08 +0200

    token_prices.h

---
+++ token_prices.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <string>
+
+void setPriceStatHandler(std::function<void(int)> h);
+
+int         getDecimals(const std::string& token);
+std::string getSymbol(const std::string& addr);
+uint64_t    getPriceNanos(const std::string& token);
+double      getPoolLiquidityUsd(const std::string& token);
+void        ensureNativePrice();
+
+void loadTokenCache();
+void loadPairCache();
+void saveTokenMetadata(const std::string& addr, const std::string& symbol, int decimals);
+void saveTokenPrice(const std::string& addr, uint64_t priceNanos);
+void savePriceHistory(const std::string& addr, uint64_t priceNanos);
+void cleanupPriceHistory();
+void seedWalletTokensFromTrades();
