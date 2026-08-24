8c18467c838ad12afc4cf6cc9fa5f5544d1572f0
vadymdota2-rgb <vadymdota2@gmail.com>
2026-08-24 05:01:03 +0200

    wallet_menu.h
    Removed PortfolioItem struct and related functions.

--- wallet_menu.h
+++ wallet_menu.h
@@ -32,16 +32,8 @@ UIMessage buildWalletsList(const std::string& chatId, int page = 1);
 UIMessage buildRemoveConfirm(const std::string& chatId, const std::string& address, const std::string& label, Lang lang);
 }
 
-struct PortfolioItem {
-    std::string symbol;
-    std::string amount;
-    cpp_int usdNanos = 0;
-};
-
-std::vector<std::string> getWalletTokens(const std::string& wallet, int limit);
 bool getNativeBalance(const std::string& wallet, cpp_int& out);
 bool getTokenBalance(const std::string& token, const std::string& wallet, cpp_int& out);
-void forgetWalletToken(const std::string& wallet, const std::string& token);
 int getDecimals(const std::string& addr);
 std::string getSymbol(const std::string& addr);
 uint64_t getPriceNanos(const std::string& token);
@@ -49,7 +41,6 @@ double getPoolLiquidityUsd(const std::string& token);
 
 namespace TelegramUI {
 UIMessage buildAccountMenu(const std::string& chatId);
-UIMessage buildHoldCard(const std::string& chatId, const std::string& address);
 }
 
 void startAddWalletFlow(const std::string& chatId, long long messageId);
