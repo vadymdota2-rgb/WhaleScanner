1f15932f0f19ff564b9aede8b3ddb3c9125611bd
vadymdota2-rgb <vadymdota2@gmail.com>
2026-08-30 00:50:35 +0200

    hyperliquid.h

--- hyperliquid.h
+++ hyperliquid.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <string>
-#include <vector>
 #include <cstdint>
+#include <string>
+#include <utility>
+#include <vector>
 
 std::vector<std::string> hlWatchedAddresses();
 
